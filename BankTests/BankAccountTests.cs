c85dda9f8e2b95efa3c348cadb36022deac3d162
truthanb <ben.truthan.w@gmail.com>
2016-06-07 10:44:33 -0400

    add credit test, and debit freeze test

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BankAccountNS;
 
@@ -68,8 +68,57 @@ namespace BankTests
             }
             Assert.Fail("No exception was thrown");
         }
+
+        [TestMethod]
+        public void Debit_WhenAccountIsFrozen_ShouldThrowException()
+        {
+            //arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 5.00;
+            BankAccount account = new BankAccount("Ben Truthan", beginningBalance);
+
+            //act
+            try
+            {
+                account.FreezeAccount();
+                account.Debit(debitAmount);
+            }
+            catch(Exception e)
+            {
+                //assert
+                StringAssert.Contains(e.Message, "Account frozen");
+                return;
+            }
+            Assert.Fail("No exception was thrown");
+        }
         #endregion
 
+        #region Credit Tests
+        [TestMethod]
+        public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //arrange
+            double beginningBalance = 11.99;
+            double creditAmount = -.50;
+            BankAccount account = new BankAccount("Ben Truthan", beginningBalance);
+
+            //act
+            try
+            {
+                account.Credit(creditAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                //assert
+                StringAssert.Contains(e.Message, BankAccount.CreditAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown");
+
+        }
+
+
+        #endregion
 
     }
 }
